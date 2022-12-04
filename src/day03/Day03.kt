d107d7e4c7498bdd27e531c2db8b0da4a90351d1
Maksim Ivanov <ivanovmeya@gmail.com>
2022-12-04 16:16:32 +0400

    fix

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -58,7 +58,7 @@ fun main() {
         }
     }
 
-    val testInput = readInput("day03/input_test.txt")
+    val testInput = readInput("day03/input_test")
     val test1Result = part1(testInput)
     val test2Result = part2(testInput)
 
